bd11166bc37008a65ed566b46da889e17f6e3b31
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2022-10-23 19:09:38 +0100

    Debugging

--- pipelines/development.sh
+++ pipelines/development.sh
@@ -5,13 +5,20 @@ trap "pipeline_notification 'fail' && exit 1" ERR
 
 echo "[INFO] Installing serverless framework and required plugins"
 npm install -g serverless
-cd backend && npm install
+cd backend
+npm install
+cd ..
+
+echo "[INFO] Deploying backend"
+make deploy-backend
 
 echo "[INFO] Installing terraform"
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
-make deploy-backend
-cd terraform && make init
-cd terraform && make apply
+echo "[INFO] Deploying terraform"
+cd terraform
+make init
+make apply
+cd ..
