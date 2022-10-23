f90a4a99afa8024bb5a3051a126e8fbd7525378c
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2022-10-23 21:56:36 +0100

    Debugging

--- pipelines/development.sh
+++ pipelines/development.sh
@@ -9,7 +9,7 @@ cd backend
 npm install
 cd ..
 
-echo "[INFO] Deploying backend"
+# Deploy the backend using serverless framework
 make deploy-backend
 
 echo "[INFO] Installing terraform"
@@ -17,7 +17,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
-echo "[INFO] Deploying terraform"
+# Deploy the terraform
 cd terraform
 make init
 make apply
