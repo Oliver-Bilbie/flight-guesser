3fb8ae188e4d4fe92cc74fac9dab4f480ae356e1
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2022-11-05 21:36:56 +0000

    Debugging

--- pipelines/deploy.sh
+++ pipelines/deploy.sh
@@ -3,25 +3,25 @@
 set -Eeo pipefail
 trap "echo 'fail' && exit 1" ERR
 
-echo "[INFO] Installing terraform"
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+# echo "[INFO] Installing terraform"
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum -y install terraform
 
-# Deploy the terraform
-cd terraform
-make init
-make apply
-cd ..
+# # Deploy the terraform
+# cd terraform
+# make init
+# make apply
+# cd ..
 
-echo "[INFO] Installing serverless framework and required plugins"
-npm install -g serverless
-cd backend
-npm install
-cd ..
+# echo "[INFO] Installing serverless framework and required plugins"
+# npm install -g serverless
+# cd backend
+# npm install
+# cd ..
 
-# Deploy the backend using serverless framework
-make deploy-backend
+# # Deploy the backend using serverless framework
+# make deploy-backend
 
 # Build and deploy the frontend to S3
 make build-frontend
@@ -30,4 +30,4 @@ if [ $STAGE == "prd" ]; then
 else
     BUCKET_NAME="s3://dev.oliver-bilbie.co.uk"
 fi
-aws s3 cp --recursive frontend/build/* $BUCKET_NAME
+aws s3 cp frontend/build $BUCKET_NAME --recursive 
