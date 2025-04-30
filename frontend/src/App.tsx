0e10e2564d872b360e0d06c077edcef315bbfd2e
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-04-30 18:16:25 +0100

    Updated app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
-import logo from "/favicon.ico";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ScoreDisplay from "./components/ScoreDisplay/ScoreDisplay";
+import MakeGuess from "./components/MakeGuess/MakeGuess";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
+      <ScoreDisplay />
+      <MakeGuess />
       <Footer />
     </>
   );
