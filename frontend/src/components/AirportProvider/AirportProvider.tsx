91216b1c9722f8e824665e7e67a9d632fdd5ba9e
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-05-01 17:56:40 +0100

    Added error handling for airport provider

--- frontend/src/components/AirportProvider/AirportProvider.tsx
+++ frontend/src/components/AirportProvider/AirportProvider.tsx
@@ -1,8 +1,14 @@
 import { FC, ReactElement, ReactNode, useEffect, useState } from "react";
 import AirportContext from "./AirportContext";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
-import { Airport, AirportApiResponse } from "../../utils/types";
+import {
+  Airport,
+  AirportApiResponse,
+  emptyError,
+  ErrorData,
+} from "../../utils/types";
 import { AIRPORTS_ENDPOINT } from "../../utils/endpoints";
+import MessageDisplay from "../MessageDisplay/MessageDisplay";
 
 interface AirportProviderProps {
   children?: ReactNode;
@@ -13,25 +19,44 @@ const AirportProvider: FC<AirportProviderProps> = ({
 }): ReactElement => {
   const [airports, setAirports] = useState<Airport[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [error, setError] = useState<ErrorData>(emptyError);
 
   async function getAirports() {
     try {
       const response = await fetch(AIRPORTS_ENDPOINT);
-      const body = await response.json();
-      const airports = body.map((airport: AirportApiResponse) => {
-        return {
-          name: airport.name,
-          iata: airport.iata,
-          position: {
-            lat: airport.lat,
-            lon: airport.lon,
-          },
-        };
-      });
-      setAirports(airports);
+
+      if (response.ok) {
+        const body: AirportApiResponse[] = await response.json();
+        const airports: Airport[] = body.map((airport) => {
+          return {
+            name: airport.name,
+            iata: airport.iata,
+            position: {
+              lat: airport.lat,
+              lon: airport.lon,
+            },
+            icao: airport.icao,
+            country: airport.country,
+            city: null,
+          };
+        });
+        setAirports(airports);
+      } else {
+        throw "API request was unsuccessful";
+      }
     } catch (error) {
-      setErrorMessage(String(error));
+      setError({
+        show: true,
+        title: "Unable to load airports",
+        message:
+          "Something went wrong when trying to contact the server. Please try again later.",
+        continueText: "Retry",
+        onContinue: () => {
+          setError(emptyError);
+          getAirports();
+        },
+      });
+      console.error(error);
     } finally {
       setIsLoading(false);
     }
@@ -43,8 +68,15 @@ const AirportProvider: FC<AirportProviderProps> = ({
 
   if (isLoading) {
     return <LoadingSpinner />;
-  } else if (errorMessage.length > 0) {
-    return <h1>{errorMessage}</h1>;
+  } else if (error.show) {
+    return (
+      <MessageDisplay
+        title={error.title}
+        message={error.message}
+        continueText={error.continueText}
+        onContinue={error.onContinue}
+      />
+    );
   } else {
     return (
       <AirportContext.Provider value={airports}>
