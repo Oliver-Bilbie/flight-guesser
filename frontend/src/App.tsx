3be1549ee0297336918970edf7d3a784c0031737
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-05-02 00:25:45 +0100

    Added settings button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,26 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ScoreDisplay from "./components/ScoreDisplay/ScoreDisplay";
 import MakeGuess from "./components/MakeGuess/MakeGuess";
+import SettingsMenu from "./components/SettingsMenu/SettingsMenu";
+import { useState } from "react";
+import SettingsButton from "./components/SettingsButton/SettingsButton";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(false);
+
   return (
     <>
       <Header />
       <ScoreDisplay />
-      <MakeGuess />
+
+      {showSettings ? (
+        <SettingsMenu onClose={() => setShowSettings(false)} />
+      ) : (
+        <MakeGuess />
+      )}
+
+      <SettingsButton onClick={() => setShowSettings(true)} />
+
       <Footer />
     </>
   );
