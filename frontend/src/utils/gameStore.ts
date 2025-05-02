8dbe50b35988a5c77d3aad447f51e2dd3fc9adc0
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-05-02 01:30:27 +0100

    Made game state persist across sessions

--- frontend/src/utils/gameStore.ts
+++ frontend/src/utils/gameStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Rules } from "./types";
 
 type GameStore = {
@@ -10,34 +11,41 @@ type GameStore = {
   setRules: (rules: Rules) => void;
 };
 
-export const useGameStore = create<GameStore>((set, get) => ({
-  score: 0,
-  guessedFlights: [],
-  rules: { useOrigin: true, useDestination: true },
+export const useGameStore = create<GameStore>()(
+  persist(
+    (set, get) => ({
+      score: 0,
+      guessedFlights: [],
+      rules: { useOrigin: true, useDestination: true },
 
-  checkIfGuessed: (flightId) => {
-    const { guessedFlights } = get();
-    return guessedFlights.includes(flightId);
-  },
+      checkIfGuessed: (flightId) => {
+        const { guessedFlights } = get();
+        return guessedFlights.includes(flightId);
+      },
 
-  handleGuessResult: (points, flightId) =>
-    set((state) => {
-      // Don't process blocked flights
-      if (flightId === "Blocked-None-None") {
-        return state;
-      }
+      handleGuessResult: (points, flightId) =>
+        set((state) => {
+          // Don't process blocked flights
+          if (flightId === "Blocked-None-None") {
+            return state;
+          }
 
-      // Don't add points if already guessed
-      const { checkIfGuessed: alreadyGuessed } = get();
-      if (alreadyGuessed(flightId)) {
-        return state;
-      }
+          // Don't add points if already guessed
+          const { checkIfGuessed: alreadyGuessed } = get();
+          if (alreadyGuessed(flightId)) {
+            return state;
+          }
 
-      return {
-        score: state.score + points,
-        guessedFlights: [...state.guessedFlights, flightId],
-      };
+          return {
+            score: state.score + points,
+            guessedFlights: [...state.guessedFlights, flightId],
+          };
+        }),
+
+      setRules: (rules) => set({ rules }),
     }),
-
-  setRules: (rules) => set({ rules }),
-}));
+    {
+      name: "gameState",
+    },
+  ),
+);
